ec395337868c8df2132053cc943bef3a9b090360
synaric.yong <synaric.yong@bcc.global>
2024-08-16 17:18:35 +0800

    修改host

--- modules/config.js
+++ modules/config.js
@@ -1,2 +1,3 @@
-export const HOST = "http://172.16.89.91";
+export const HOST = "https://meeting-manage-dev.businessconnectchina.com:11443";
+// export const HOST = "http://172.16.89.91";
 // export const HOST = "http://192.168.1.109:81";
