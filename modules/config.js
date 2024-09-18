a295a9ba7e84e82eda3dab704a5b42d3104a0bf0
synaric.yong <synaric.yong@bcc.global>
2024-09-18 10:29:07 +0800

    迁移目录

--- modules/config.js
+++
@@ -1,5 +0,0 @@
-// export const HOST = "https://meeting-manage-dev.businessconnectchina.com:11443";
-// export const HOST = "https://meeting-manage-dev.businessconnectchina.com:11443/";
-// export const HOST = "http://172.16.89.91";
-// export const HOST = "http://192.168.1.109:81";
-export const HOST = "http://172.16.89.161:82";
